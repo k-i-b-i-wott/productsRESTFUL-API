a5bf21a27cf9aafffbf6bd3bd9db93dde4ed504c
Kibiwott <bk.kibiwott@gmail.com>
2025-04-05 20:01:47 +0300

    Added the product routers to the application

--- index.js
+++ index.js
@@ -1,168 +1,26 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
-import validateProducts from "./middleware/validateProducts.js";
+// import { PrismaClient } from "@prisma/client";
+
+import {productsRouters, productsOnOffer} from "./routes/productsRouters.js";
 
 const app = express();
-const client = new PrismaClient();
+// const client = new PrismaClient();
 app.use(express.json());
 
+app.use("/products", productsRouters);
+app.use("/offer", productsOnOffer);
 
-app.get("/products",async (_req, res) => {
-  try {
-     const allProducts = await client.products.findMany();
-    res.status(200).json({
-      status: "success",
-      message: "Products fetched successfully",
-      data: allProducts,
-    });
+// app.get("/products",productsRouters);
+// app.get("/products/:productId",);
 
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      status: "error",
-      message: "Error fetching products",
-    });    
-  }
+// app.get("/offer",productsRouters);
 
+// app.post("/products", );
 
-});
-app.get("/products/:productId", async (req, res) => {  
-  const {productId} =req.params;
+// app.patch("/products/:productId",);
 
-  try {
-    const product = await client.products.findFirst({
-      where: {
-        productId,
-      },
-    });
-
-    if (!product) {
-      return res.status(404).json({
-        status: "error",
-        message: "Product not found",
-      });
-    }
-    res.status(200).json({
-      status: "success",
-      message: "Product fetched successfully",
-      data: product,
-    });
-
-}catch (error) {
-  console.log(error);
-  res.status(500).json({
-    status: "error",
-    message: "Error  fetching the product",
-  });    
-}
-
-});
-
-app.get("/offer", async (_req,res)=> {
-  try {
-    const productsOnOffer = await client.products.findMany({
-      where: {
-        isOnOffer: true,
-      },
-    });
-    res.status(200).json({
-      status: "success",
-      message: "Products on offer fetched successfully",
-      data: productsOnOffer,
-    });
-    
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      status: "error",
-      message: "Error fetching products on offer",
-    });
-    
-  }
-
-});
-
-
-
-app.post("/products", validateProducts, async (req, res) => {
-  const { productTitle, productDescription, unitsLeft, pricePerUnit } =
-    req.body;
-  try {
-    const createdProduct = await client.products.create({
-      data: {
-        productTitle,
-        productDescription,
-        unitsLeft,
-        pricePerUnit,
-      },
-    });
-    res.status(201).json({
-      status: "success",
-      message: "Product created successfully",
-      task: createdProduct,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: "Error creating product",
-    });
-  }
-});
-
-app.patch("/products/:productId", async (req, res) => {
-  const { productId } = req.params;
-  const { productTitle, productDescription, unitsLeft, pricePerUnit, isOnOffer } =
-    req.body;
-
-  try {
-    const updatedProduct = await client.products.update({
-      where: {
-        productId,
-      },
-      data: {
-        productTitle: productTitle&& productTitle,
-        productDescription: productDescription && productDescription,
-        unitsLeft: unitsLeft && unitsLeft,
-        pricePerUnit: pricePerUnit && pricePerUnit,
-        isOnOffer: isOnOffer && isOnOffer,
-      },    
-    });
-    res.status(200).json({
-      status: "success",
-      message: "Product updated successfully",
-      data: updatedProduct,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: "Error updating product",
-    });
-  }
-});
-
-
-app.delete("/products/:productId", async (req, res) => {
-  const { productId } = req.params;
-
-  try {
-    const deletedProduct = await client.products.delete({
-      where: {
-        productId,
-      },
-    });
-    res.status(200).json({
-      status: "success",
-      message: "Product deleted successfully",
-      data: deletedProduct,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: "Error deleting product",
-    });
-  }
-}
-);
+// app.delete("/products/:productId",
+// );
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
